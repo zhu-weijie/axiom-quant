53fcf5e9411b9b7ba0d5da5f65a8bc08abb474a2
zhuwj <byzhubaiyuan@gmail.com>
2025-07-08 10:17:47 +0800

    Make fetch_market_data robust

--- src/axiomquant/pipelines/data_loader.py
+++ src/axiomquant/pipelines/data_loader.py
@@ -3,40 +3,47 @@ import yfinance as yf
 
 
 def fetch_market_data(tickers: list[str], period: str = "5y") -> pd.DataFrame | None:
+    if not tickers:
+        return None
+
     try:
-        data = yf.download(tickers, period=period, group_by="ticker")
+        data = yf.download(tickers, period=period, auto_adjust=False, group_by="ticker")
+
         if data.empty:
-            print("No data downloaded. Check ticker symbols and period.")
             return None
 
-        if len(tickers) == 1:
-            data["Ticker"] = tickers[0]
-        else:
-            data = data.stack(level=0).rename_axis(["Date", "Ticker"]).reset_index()
+        data = (
+            data.stack(level=0, future_stack=True)
+            .rename_axis(["Date", "Ticker"])
+            .reset_index()
+        )
 
         data.rename(
             columns={
+                "Date": "trade_date",
+                "Ticker": "ticker",
                 "Open": "open_price",
                 "High": "high_price",
                 "Low": "low_price",
                 "Close": "close_price",
                 "Volume": "volume",
-                "Date": "trade_date",
             },
             inplace=True,
         )
 
-        return data[
-            [
-                "trade_date",
-                "Ticker",
-                "open_price",
-                "high_price",
-                "low_price",
-                "close_price",
-                "volume",
-            ]
+        final_columns = [
+            "trade_date",
+            "ticker",
+            "open_price",
+            "high_price",
+            "low_price",
+            "close_price",
+            "volume",
         ]
+
+        data.dropna(subset=final_columns, inplace=True)
+
+        return data[final_columns]
 
     except Exception as e:
         print(f"An error occurred while fetching data: {e}")
